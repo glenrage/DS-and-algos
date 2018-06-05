5afcede8240d8b1b3ee94165c2c3e8da9fef5966
glenrage <phamgalang@gmail.com>
2018-06-05 13:33:56 -0700

    added misc func

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -46,16 +46,57 @@ class LinkedList {
     if (!this.head) return;
     this.head = this.head.next;
   }
-  removeLast() {
-    let node = this.head;
-    while (node) {, ea
 
-    
-      if (!node.next) {
-        node = node.next     ;
-      }
-      break;
+  removeLast() {
+    if (!this.head) {
+      return;
     }
+    if (!this.head.next) {
+      this.head = null;
+      return;
+    }
+    let previous = this.head;
+    let node = this.head.next;
+    while (node.next) {
+      previous = node;
+      node = node.next;
+    }
+    previous.next = null;
+  }
+
+  insertLast(data) {
+    const last = this.getLast();
+    if (last) {
+      last.next = new Node(data);
+    } else {
+      this.head = new Node(data);
+    }
+  }
+
+  getAt(index) {
+    let counter = 0;
+    let node = this.head;
+    while (node) {
+      if (counter === index) {
+        return node;
+      }
+      counter++;
+      node = node.next;
+    }
+    return null;
+  }
+
+  removeAt(index) {
+    if (!this.head) {
+      return;
+    }
+    if (index === 0) {
+      this.head = this.head.next;
+      return;
+    }
+    const previous = this.getAt(index - 1);
+    if (!previous || !previous.next) return;
+    previous.next = previous.next.next;
   }
 }
 
