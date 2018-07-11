f0ebfa86cdfa3c6869b02e8152fa7872778586d3
glenrage <phamgalang@gmail.com>
2018-07-10 17:38:57 -0700

    added misc

--- interview.js
+++ interview.js
@@ -1,8 +1,8 @@
-function reverseArrayInPlace(arr) {
-  for (var i = 0; i < arr.length / 2; i++) {
-    var temp = arr[i];
-    arr[i] = arr[arr.length - 1 - i];
-    arr[arr.length - 1 - i] = temp;
-  }
-  return arr;
-}
+// function reverseArrayInPlace(arr) {
+//   for (var i = 0; i < arr.length / 2; i++) {
+//     var temp = arr[i];
+//     arr[i] = arr[arr.length - 1 - i];
+//     arr[arr.length - 1 - i] = temp;
+//   }
+//   return arr;
+// }
